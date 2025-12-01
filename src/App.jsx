83500b8a71a45d0c553b475421a71e123335a65d
faseed <mohamedfaseeda@gmail.com>
2025-12-01 13:49:58 +0530

    app jsx  fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 // Home Page Component
-function HomePage() {
+function App() {
   return (
     <div className="app-container">
       <Navbar />
