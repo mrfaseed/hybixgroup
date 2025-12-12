907c149d43fa003bf4867c853bcf27dd077f50c9
mohammedshaban <imshabanoffl@gmail.com>
2025-12-12 12:13:40 +0530

    contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import './Contact.css'
+import emailAnimation from '../assets/Email.lottie';
+
 const Contact = () => {
   return (
     <div className="contact_us_green">
       <div className="responsive-container-block big-container">
         <div className="responsive-container-block container">
-          
+
           {/* LEFT SIDE FORM */}
           <div
-            className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-7 wk-ipadp-10 line"
+            className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-6 wk-ipadp-10 line"
             id="i69b-2"
           >
             <form className="form-box">
@@ -65,85 +68,16 @@ const Contact = () => {
 
           {/* RIGHT SIDE INFO */}
           <div
-            className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-5 wk-ipadp-10"
+            className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-6 wk-ipadp-10"
             id="ifgi"
+            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
           >
-            <div className="container-box">
-              <div className="text-content">
-                <p className="text-blk contactus-head">Contact us</p>
-                <p className="text-blk contactus-subhead">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna al
-                  iqua. Ut enim
-                </p>
-              </div>
-
-              <div className="workik-contact-bigbox">
-                <div className="workik-contact-box">
-                  <div className="phone text-box">
-                    <img
-                      className="contact-svg"
-                      src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ET21.jpg"
-                      alt=""
-                    />
-                    <p className="contact-text">+1258 3258 5679</p>
-                  </div>
-
-                  <div className="address text-box">
-                    <img
-                      className="contact-svg"
-                      src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ET22.jpg"
-                      alt=""
-                    />
-                    <p className="contact-text">hello@workik.com</p>
-                  </div>
-
-                  <div className="mail text-box">
-                    <img
-                      className="contact-svg"
-                      src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ET23.jpg"
-                      alt=""
-                    />
-                    <p className="contact-text">102 street, y cross 485656</p>
-                  </div>
-                </div>
-
-                <div className="social-media-links">
-                  <a href="">
-                    <img
-                      className="social-svg"
-                      id="is9ym"
-                      src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg"
-                      alt=""
-                    />
-                  </a>
-                  <a href="">
-                    <img
-                      className="social-svg"
-                      id="i706n"
-                      src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg"
-                      alt=""
-                    />
-                  </a>
-                  <a href="">
-                    <img
-                      className="social-svg"
-                      id="ib9ve"
-                      src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
-                      alt=""
-                    />
-                  </a>
-                  <a href="">
-                    <img
-                      className="social-svg"
-                      id="ie9fx"
-                      src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
-                      alt=""
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
+            <DotLottieReact
+              src={emailAnimation}
+              loop
+              autoplay
+              style={{ width: '100%', height: '100%' }}
+            />
           </div>
 
         </div>
