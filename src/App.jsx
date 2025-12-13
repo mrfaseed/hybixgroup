4220033800c415ba9ff1c753a21d1d55aa26ddaf
Esan-8055 <jegatheesh8055@gmail.com>
2025-12-13 13:24:15 +0530

    Fixed duplicate import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const NewsMedia = React.lazy(() => import('./components/NewsMedia'));
 const HomeCity = React.lazy(() => import('./components/HomeCity'));
 const CustomerReviews = React.lazy(() => import('./components/CustomerReviews'));
 const LearnMore = React.lazy(() => import('./components/LearnMore'));
-const ConsultationPage = React.lazy(() => import('./components/ConsultationPage'));
+
 const CaseStudies = React.lazy(() => import('./components/CaseStudies'));
 
 // Home Page Component
